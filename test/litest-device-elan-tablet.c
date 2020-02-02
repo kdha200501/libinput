kdha200501/libinput

commit b17fca59bd7badb8e68359d83fc51fad74967dc0

tablet: fix double proximity out on slow proximity out pens
Where the proximity out event is delayed by the kernel, libinput would cause
an extra proxmity in-out after the forced proximity out event.

Event sequence is basically (k: kernel, l: libinput)

k: tablet axis events
l: tablet axis events
k: nothing for $proximity timer milliseconds
l: tablet proximity out
k: proximity out event
l: proximity in event
l: proximity out event

Fixes #306

Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

src/evdev-tablet.c
@@ -806,6 +806,7 @@ tablet_process_key(struct tablet_dispatch *tablet,
 	case BTN_TOOL_MOUSE:
 	case BTN_TOOL_LENS:
 		type = tablet_evcode_to_tool(e->code);
+		tablet_set_status(tablet, TABLET_TOOL_UPDATED);
 		if (e->value)
 			tablet->tool_state |= bit(type);
 		else
@@ -1721,13 +1722,22 @@ tablet_update_tool_state(struct tablet_dispatch *tablet,
 	 *   BTN_TOOL_PEN and the state for the tool was 0, this device will
 	 *   never send the event.
 	 * We don't do this for pure button events because we discard those.
+	 *
+	 * But: on some devices the proximity out is delayed by the kernel,
+	 * so we get it after our forced prox-out has triggered. In that
+	 * case we need to just ignore the change.
 	 */
-	if (tablet_has_status(tablet, TABLET_AXES_UPDATED) &&
-	    (tablet->quirks.proximity_out_forced ||
-	     (tablet->tool_state == 0 &&
-	      tablet->current_tool.type == LIBINPUT_TOOL_NONE))) {
-		tablet->tool_state = bit(LIBINPUT_TABLET_TOOL_TYPE_PEN);
-		tablet->quirks.proximity_out_forced = false;
+	if (tablet_has_status(tablet, TABLET_AXES_UPDATED)) {
+		if (tablet->quirks.proximity_out_forced) {
+			if (!tablet_has_status(tablet, TABLET_TOOL_UPDATED) ||
+			    tablet->tool_state)
+				tablet->tool_state = bit(LIBINPUT_TABLET_TOOL_TYPE_PEN);
+			tablet->quirks.proximity_out_forced = false;
+		} else if (tablet->tool_state == 0 &&
+			    tablet->current_tool.type == LIBINPUT_TOOL_NONE) {
+			tablet->tool_state = bit(LIBINPUT_TABLET_TOOL_TYPE_PEN);
+			tablet->quirks.proximity_out_forced = false;
+		}
 	}
 
 	if (tablet->tool_state == tablet->prev_tool_state)
@@ -1886,6 +1896,7 @@ tablet_reset_state(struct tablet_dispatch *tablet)
 	memcpy(&tablet->prev_button_state,
 	       &tablet->button_state,
 	       sizeof(tablet->button_state));
+	tablet_unset_status(tablet, TABLET_TOOL_UPDATED);
 }
 
 static void

test/test-tablet.c
@@ -1506,6 +1506,32 @@ START_TEST(proximity_range_button_release)
 }
 END_TEST
 
+START_TEST(proximity_out_slow_event)
+{
+	struct litest_device *dev = litest_current_device();
+	struct libinput *li = dev->libinput;
+	struct axis_replacement axes[] = {
+		{ ABS_DISTANCE, 90 },
+		{ -1, -1 }
+	};
+
+	litest_tablet_proximity_in(dev, 10, 10, axes);
+	litest_tablet_motion(dev, 12, 12, axes);
+	litest_drain_events(li);
+
+	litest_timeout_tablet_proxout();
+	libinput_dispatch(li);
+
+	/* The forced prox out */
+	litest_assert_tablet_proximity_event(li,
+					     LIBINPUT_TABLET_TOOL_PROXIMITY_STATE_OUT);
+	litest_assert_empty_queue(li);
+
+	litest_tablet_proximity_out(dev);
+	litest_assert_empty_queue(li);
+}
+END_TEST
+
 START_TEST(proximity_out_on_delete)
 {
 	struct libinput *li = litest_create_context();
@@ -5651,6 +5677,8 @@ TEST_COLLECTION(tablet)
 	litest_add("tablet:proximity", proximity_range_button_click, LITEST_TABLET | LITEST_DISTANCE | LITEST_TOOL_MOUSE, LITEST_ANY);
 	litest_add("tablet:proximity", proximity_range_button_press, LITEST_TABLET | LITEST_DISTANCE | LITEST_TOOL_MOUSE, LITEST_ANY);
 	litest_add("tablet:proximity", proximity_range_button_release, LITEST_TABLET | LITEST_DISTANCE | LITEST_TOOL_MOUSE, LITEST_ANY);
+	litest_add("tablet:proximity", proximity_out_slow_event, LITEST_TABLET | LITEST_DISTANCE, LITEST_ANY);
+
 	litest_add_no_device("tablet:proximity", proximity_out_on_delete);
 	litest_add("tablet:button", button_down_up, LITEST_TABLET, LITEST_ANY);
 	litest_add("tablet:button", button_seat_count, LITEST_TABLET, LITEST_ANY);

src/evdev-tablet.h
@@ -38,13 +38,14 @@ enum tablet_status {
 	TABLET_AXES_UPDATED		= bit(0),
 	TABLET_BUTTONS_PRESSED		= bit(1),
 	TABLET_BUTTONS_RELEASED		= bit(2),
-	TABLET_TOOL_IN_CONTACT		= bit(3),
-	TABLET_TOOL_LEAVING_PROXIMITY	= bit(4),
-	TABLET_TOOL_OUT_OF_PROXIMITY	= bit(5),
-	TABLET_TOOL_ENTERING_PROXIMITY	= bit(6),
-	TABLET_TOOL_ENTERING_CONTACT	= bit(7),
-	TABLET_TOOL_LEAVING_CONTACT	= bit(8),
-	TABLET_TOOL_OUT_OF_RANGE	= bit(9),
+	TABLET_TOOL_UPDATED		= bit(3),
+	TABLET_TOOL_IN_CONTACT		= bit(4),
+	TABLET_TOOL_LEAVING_PROXIMITY	= bit(5),
+	TABLET_TOOL_OUT_OF_PROXIMITY	= bit(6),
+	TABLET_TOOL_ENTERING_PROXIMITY	= bit(7),
+	TABLET_TOOL_ENTERING_CONTACT	= bit(8),
+	TABLET_TOOL_LEAVING_CONTACT	= bit(9),
+	TABLET_TOOL_OUT_OF_RANGE	= bit(10),
 };
 
 struct button_state {

commit f71efa3af1c548677e3606e223a54d0fea7aa25e

tablet: fix the handling of axis updates after a forced proximity out
Where a pen was forced out of proximity and an eraser came into proximity
without axis updates on the prox-in, subsequent axis updates would trigger the
pen back into proximity. This resulted in two tools in proximity at once
though the new pen never went out of proximity

This would trigger crashes in various compositors/applications, see
https://github.com/xournalpp/xournalpp/issues/1141#issuecomment-578362497

The cause was a wrong condition introduced in b17fca59bd. We only need to
force the pen bit on if the current tool state is currently zero and no tool
update was sent with the axis event. In our case, the tool state is nonzero
already (eraser) and we can skip this bit.

Fixes #418

Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

meson.build
@@ -785,6 +785,7 @@ if get_option('tests')
 		'test/litest-device-dell-canvas-totem.c',
 		'test/litest-device-dell-canvas-totem-touch.c',
 		'test/litest-device-elantech-touchpad.c',
+		'test/litest-device-elan-tablet.c',
 		'test/litest-device-generic-singletouch.c',
 		'test/litest-device-gpio-keys.c',
 		'test/litest-device-huion-pentablet.c',

src/evdev-tablet.c
@@ -1755,8 +1755,8 @@ tablet_update_tool_state(struct tablet_dispatch *tablet,
 	 */
 	if (tablet_has_status(tablet, TABLET_AXES_UPDATED)) {
 		if (tablet->quirks.proximity_out_forced) {
-			if (!tablet_has_status(tablet, TABLET_TOOL_UPDATED) ||
-			    tablet->tool_state)
+			if (!tablet_has_status(tablet, TABLET_TOOL_UPDATED)  &&
+			    !tablet->tool_state)
 				tablet->tool_state = bit(LIBINPUT_TABLET_TOOL_TYPE_PEN);
 			tablet->quirks.proximity_out_forced = false;
 		} else if (tablet->tool_state == 0 &&

test/litest-device-elan-tablet.c
@@ -1,0 +1,103 @@
+/*
+ * Copyright © 2020 Red Hat, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+#include "config.h"
+
+#include "litest.h"
+#include "litest-int.h"
+
+static struct input_event proximity_in[] = {
+	{ .type = EV_ABS, .code = ABS_X, .value = LITEST_AUTO_ASSIGN },
+	{ .type = EV_ABS, .code = ABS_Y, .value = LITEST_AUTO_ASSIGN },
+	{ .type = EV_ABS, .code = ABS_PRESSURE, .value = LITEST_AUTO_ASSIGN },
+	{ .type = EV_KEY, .code = BTN_TOOL_PEN, .value = 1 },
+	{ .type = EV_SYN, .code = SYN_REPORT, .value = 0 },
+	{ .type = -1, .code = -1 },
+};
+
+static struct input_event proximity_out[] = {
+	{ .type = EV_KEY, .code = BTN_TOOL_PEN, .value = 0 },
+	{ .type = EV_SYN, .code = SYN_REPORT, .value = 0 },
+	{ .type = -1, .code = -1 },
+};
+
+static struct input_event motion[] = {
+	{ .type = EV_ABS, .code = ABS_X, .value = LITEST_AUTO_ASSIGN },
+	{ .type = EV_ABS, .code = ABS_Y, .value = LITEST_AUTO_ASSIGN },
+	{ .type = EV_ABS, .code = ABS_PRESSURE, .value = LITEST_AUTO_ASSIGN },
+	{ .type = EV_SYN, .code = SYN_REPORT, .value = 0 },
+	{ .type = -1, .code = -1 },
+};
+
+static int
+get_axis_default(struct litest_device *d, unsigned int evcode, int32_t *value)
+{
+	switch (evcode) {
+	case ABS_PRESSURE:
+		*value = 100;
+		return 0;
+	}
+	return 1;
+}
+
+static struct litest_device_interface interface = {
+	.tablet_proximity_in_events = proximity_in,
+	.tablet_proximity_out_events = proximity_out,
+	.tablet_motion_events = motion,
+
+	.get_axis_default = get_axis_default,
+};
+
+static struct input_absinfo absinfo[] = {
+	{ ABS_X, 0, 18176, 0, 0, 62 },
+	{ ABS_Y, 0, 10240, 0, 0, 62 },
+	{ ABS_PRESSURE, 0, 4096, 0, 0, 0 },
+	{ .value = -1 },
+};
+
+static struct input_id input_id = {
+	.bustype = 0x18,
+	.vendor = 0x4f3,
+	.product = 0x23b9,
+	.version = 0x100,
+};
+
+static int events[] = {
+	EV_KEY, BTN_TOOL_PEN,
+	EV_KEY, BTN_TOOL_RUBBER,
+	EV_KEY, BTN_TOUCH,
+	EV_KEY, BTN_STYLUS,
+	EV_MSC, MSC_SCAN,
+	-1, -1,
+};
+
+TEST_DEVICE("elan-tablet",
+	.type = LITEST_ELAN_TABLET,
+	.features = LITEST_TABLET,
+	.interface = &interface,
+
+	.name = "ELAN2514:00 04F3:23B9",
+	.id = &input_id,
+	.events = events,
+	.absinfo = absinfo,
+)

test/litest.h
@@ -301,6 +301,7 @@ enum litest_device_type {
 	LITEST_DELL_CANVAS_TOTEM_TOUCH,
 	LITEST_WACOM_ISDV4_4200_PEN,
 	LITEST_ALPS_3FG,
+	LITEST_ELAN_TABLET,
 };
 
 #define LITEST_DEVICELESS	-2

test/test-tablet.c
@@ -2954,6 +2954,84 @@ START_TEST(tool_direct_switch_skip_tool_update)
 }
 END_TEST
 
+START_TEST(tool_direct_switch_with_forced_proxout)
+{
+	struct litest_device *dev = litest_current_device();
+	struct libinput *li = dev->libinput;
+	struct libinput_event *event;
+	struct axis_replacement axes[] = {
+		{ ABS_DISTANCE, 10 },
+		{ ABS_PRESSURE, 0 },
+		{ -1, -1 }
+	};
+
+	if (!libevdev_has_event_code(dev->evdev, EV_KEY, BTN_TOOL_RUBBER))
+		return;
+
+	litest_drain_events(li);
+
+	/* This is a *very* specific event sequence to trigger a bug:
+	   - pen proximity in
+	   - pen proximity forced out
+	   - eraser proximity in without axis movement
+	   - eraser axis move
+	 */
+
+	/* pen prox in */
+	litest_tablet_proximity_in(dev, 10, 10, axes);
+	libinput_dispatch(li);
+	event = libinput_get_event(li);
+	litest_is_proximity_event(event,
+				  LIBINPUT_TABLET_TOOL_PROXIMITY_STATE_IN);
+	libinput_event_destroy(event);
+
+	/* pen motion */
+	litest_tablet_motion(dev, 20, 30, axes);
+	libinput_dispatch(li);
+
+	event = libinput_get_event(li);
+	litest_is_tablet_event(event, LIBINPUT_EVENT_TABLET_TOOL_AXIS);
+	libinput_event_destroy(event);
+
+	/* pen forced prox out */
+	litest_timeout_tablet_proxout();
+	libinput_dispatch(li);
+
+	/* actual prox out for tablets that don't do forced prox out */
+	litest_tablet_proximity_out(dev);
+	libinput_dispatch(li);
+
+	event = libinput_get_event(li);
+	litest_is_proximity_event(event,
+				  LIBINPUT_TABLET_TOOL_PROXIMITY_STATE_OUT);
+	libinput_event_destroy(event);
+
+	/* eraser prox in without axes */
+	litest_event(dev, EV_KEY, BTN_TOOL_RUBBER, 1);
+	litest_event(dev, EV_SYN, SYN_REPORT, 0);
+	libinput_dispatch(li);
+	event = libinput_get_event(li);
+	litest_is_proximity_event(event,
+				  LIBINPUT_TABLET_TOOL_PROXIMITY_STATE_IN);
+	libinput_event_destroy(event);
+
+	/* eraser motion */
+	litest_tablet_motion(dev, 30, 40, axes);
+	litest_tablet_motion(dev, 40, 50, axes);
+	libinput_dispatch(li);
+
+	event = libinput_get_event(li);
+	litest_is_tablet_event(event, LIBINPUT_EVENT_TABLET_TOOL_AXIS);
+	libinput_event_destroy(event);
+
+	event = libinput_get_event(li);
+	litest_is_tablet_event(event, LIBINPUT_EVENT_TABLET_TOOL_AXIS);
+	libinput_event_destroy(event);
+
+	litest_assert_empty_queue(li);
+}
+END_TEST
+
 START_TEST(mouse_tool)
 {
 	struct litest_device *dev = litest_current_device();
@@ -5756,6 +5834,7 @@ TEST_COLLECTION(tablet)
 	litest_add("tablet:tool", tool_type, LITEST_TABLET, LITEST_ANY);
 	litest_add("tablet:tool", tool_in_prox_before_start, LITEST_TABLET, LITEST_TOTEM);
 	litest_add("tablet:tool", tool_direct_switch_skip_tool_update, LITEST_TABLET, LITEST_ANY);
+	litest_add("tablet:tool", tool_direct_switch_with_forced_proxout, LITEST_TABLET, LITEST_ANY);
 
 	/* Tablets hold back the proximity until the first event from the
 	 * kernel, the totem sends it immediately */