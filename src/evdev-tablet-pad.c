kdha200501/libinput

commit e2cc4456648cfd47aab154b1e6e922a233f256cf

tablet: add API for relative dials
Some tablets such as those in the XP-PEN PRO series use "dials" which
are actually scrollwheels and emit EV_REL events. These should not be
emulated as rings (which are absolute) so we must expose them as a new
tablet event.

Adds LIBINPUT_EVENT_TABLET_PAD_DIAL that work largely identical as our
high-resolution wheel events (i.e. the values are in multiples or
fractions of of 120). Currently supports two dials.

This is a lot of copy/paste from the ring axes because the interface is
virtually identical. The main difference is that dials give us a v120
value in the same manner as our scroll axes.

Notes:
- REL_DIAL is mutually exclusive with REL_WHEEL, we assume the kernel
  doesn't (at this point) give us devices with both. If this changes for
  devices with three dials (wheel + hwheel + dial) we need to add code
  for that.
- REL_DIAL does not have a high-resolution axis and we assume that any
  device with REL_WHEEL_HI_RES will also have REL_HWHEEL_HI_RES (if the
  second wheel exists).
- With dials being REL_DIAL or REL_WHEEL there is no possibility of
  detecting a finger release (the kernel does not route EV_RELs with a
  value of zero). Unless this is implemented via a side-channel - and it
  doesn't look like any hardware that supports dials does that - we
  cannot forward any information here. So unlike absolute rings we
  cannot provide a source information here.

Closes #600

Co-authored-by: Peter Hutterer <peter.hutterer@who-t.net>
Part-of: <https://gitlab.freedesktop.org/libinput/libinput/-/merge_requests/967>

src/evdev-tablet-pad.c
@@ -98,6 +98,50 @@ pad_button_set_down(struct pad_dispatch *pad,
 }
 
 static void
+pad_process_relative(struct pad_dispatch *pad,
+		     struct evdev_device *device,
+		     struct input_event *e,
+		     uint64_t time)
+{
+	switch (e->code) {
+	case REL_DIAL:
+		pad->dials.dial1 = e->value * 120;
+		pad->changed_axes |= PAD_AXIS_DIAL1;
+		pad_set_status(pad, PAD_AXES_UPDATED);
+		break;
+	case REL_WHEEL:
+		if (!pad->dials.has_hires_dial) {
+			pad->dials.dial1 = e->value * 120;
+			pad->changed_axes |= PAD_AXIS_DIAL1;
+			pad_set_status(pad, PAD_AXES_UPDATED);
+		}
+		break;
+	case REL_HWHEEL:
+		if (!pad->dials.has_hires_dial) {
+			pad->dials.dial2 = e->value * 120;
+			pad->changed_axes |= PAD_AXIS_DIAL2;
+			pad_set_status(pad, PAD_AXES_UPDATED);
+		}
+		break;
+	case REL_WHEEL_HI_RES:
+		pad->dials.dial1 = e->value;
+		pad->changed_axes |= PAD_AXIS_DIAL1;
+		pad_set_status(pad, PAD_AXES_UPDATED);
+		break;
+	case REL_HWHEEL_HI_RES:
+		pad->dials.dial2 = e->value * 120;
+		pad->changed_axes |= PAD_AXIS_DIAL2;
+		pad_set_status(pad, PAD_AXES_UPDATED);
+		break;
+	default:
+		evdev_log_info(device,
+			       "Unhandled EV_REL event code %#x\n",
+			       e->code);
+		break;
+	}
+}
+
+static void
 pad_process_absolute(struct pad_dispatch *pad,
 		     struct evdev_device *device,
 		     struct input_event *e,
@@ -217,6 +261,22 @@ pad_handle_strip(struct pad_dispatch *pad,
 }
 
 static inline struct libinput_tablet_pad_mode_group *
+pad_dial_get_mode_group(struct pad_dispatch *pad,
+			unsigned int dial)
+{
+	struct libinput_tablet_pad_mode_group *group;
+
+	list_for_each(group, &pad->modes.mode_group_list, link) {
+		if (libinput_tablet_pad_mode_group_has_dial(group, dial))
+			return group;
+	}
+
+	assert(!"Unable to find dial mode group");
+
+	return NULL;
+}
+
+static inline struct libinput_tablet_pad_mode_group *
 pad_ring_get_mode_group(struct pad_dispatch *pad,
 			unsigned int ring)
 {
@@ -263,6 +323,26 @@ pad_check_notify_axes(struct pad_dispatch *pad,
 	if (pad->have_abs_misc_terminator &&
 	    libevdev_get_event_value(device->evdev, EV_ABS, ABS_MISC) == 0)
 		send_finger_up = true;
+
+	/* Unlike the ring axis we don't get an event when we release
+	 * so we can't set a source */
+	if (pad->changed_axes & PAD_AXIS_DIAL1) {
+		group = pad_dial_get_mode_group(pad, 0);
+		tablet_pad_notify_dial(base,
+				       time,
+				       0,
+				       pad->dials.dial1,
+				       group);
+	}
+
+	if (pad->changed_axes & PAD_AXIS_DIAL2) {
+		group = pad_dial_get_mode_group(pad, 1);
+		tablet_pad_notify_dial(base,
+				       time,
+				       1,
+				       pad->dials.dial2,
+				       group);
+	}
 
 	if (pad->changed_axes & PAD_AXIS_RING1) {
 		value = pad_handle_ring(pad, device, ABS_WHEEL);
@@ -473,6 +553,8 @@ pad_flush(struct pad_dispatch *pad,
 	memcpy(&pad->prev_button_state,
 	       &pad->button_state,
 	       sizeof(pad->button_state));
+	pad->dials.dial1 = 0;
+	pad->dials.dial2 = 0;
 }
 
 static void
@@ -484,6 +566,9 @@ pad_process(struct evdev_dispatch *dispatch,
 	struct pad_dispatch *pad = pad_dispatch(dispatch);
 
 	switch (e->type) {
+	case EV_REL:
+		pad_process_relative(pad, device, e, time);
+		break;
 	case EV_ABS:
 		pad_process_absolute(pad, device, e, time);
 		break;
@@ -686,6 +771,16 @@ pad_init(struct pad_dispatch *pad, struct evdev_device *device)
 	pad->status = PAD_NONE;
 	pad->changed_axes = PAD_AXIS_NONE;
 
+        /* We expect the kernel to either give us both axes as hires or neither.
+	 * Getting one is a kernel bug we don't need to care about */
+        pad->dials.has_hires_dial = libevdev_has_event_code(device->evdev, EV_REL, REL_WHEEL_HI_RES) ||
+				    libevdev_has_event_code(device->evdev, EV_REL, REL_HWHEEL_HI_RES);
+
+	if (libevdev_has_event_code(device->evdev, EV_REL, REL_WHEEL) &&
+	    libevdev_has_event_code(device->evdev, EV_REL, REL_DIAL)) {
+		log_bug_libinput(pad_libinput_context(pad), "Unsupported combination REL_DIAL and REL_WHEEL\n");
+	}
+
 	pad_init_buttons(pad, device);
 	pad_init_left_handed(device);
 	if (pad_init_leds(pad, device) != 0)
@@ -780,6 +875,26 @@ evdev_device_tablet_pad_get_num_buttons(struct evdev_device *device)
 		return -1;
 
 	return pad->nbuttons;
+}
+
+int
+evdev_device_tablet_pad_get_num_dials(struct evdev_device *device)
+{
+	int ndials = 0;
+
+	if (!(device->seat_caps & EVDEV_DEVICE_TABLET_PAD))
+		return -1;
+
+	if (libevdev_has_event_code(device->evdev, EV_REL, REL_WHEEL) ||
+	    libevdev_has_event_code(device->evdev, EV_REL, REL_DIAL)) {
+		ndials++;
+		if (libevdev_has_event_code(device->evdev,
+					    EV_REL,
+					    REL_HWHEEL))
+			ndials++;
+	}
+
+	return ndials;
 }
 
 int

meson.build
@@ -757,6 +757,7 @@ if get_option('tests')
 		'test/litest-device-generic-singletouch.c',
 		'test/litest-device-gpio-keys.c',
 		'test/litest-device-huion-pentablet.c',
+		'test/litest-device-huion-q620m-dial.c',
 		'test/litest-device-hp-wmi-hotkeys.c',
 		'test/litest-device-ignored-mouse.c',
 		'test/litest-device-keyboard.c',
@@ -792,7 +793,9 @@ if get_option('tests')
 		'test/litest-device-synaptics-t440.c',
 		'test/litest-device-synaptics-x1-carbon-3rd.c',
 		'test/litest-device-synaptics-phantomclicks.c',
+		'test/litest-device-tablet-doubledial.c',
 		'test/litest-device-tablet-mode-switch.c',
+		'test/litest-device-tablet-rel-dial.c',
 		'test/litest-device-thinkpad-extrabuttons.c',
 		'test/litest-device-trackpoint.c',
 		'test/litest-device-touch-screen.c',

src/evdev-tablet-pad-leds.c
@@ -517,6 +517,7 @@ pad_init_leds_from_libwacom(struct pad_dispatch *pad,
 
 	pad_init_mode_rings(pad, wacom);
 	pad_init_mode_strips(pad, wacom);
+	/* Note: libwacom doesn't do dials yet */
 
 out:
 	if (wacom)
@@ -546,6 +547,7 @@ pad_init_fallback_group(struct pad_dispatch *pad)
 	group->base.button_mask = -1;
 	group->base.strip_mask = -1;
 	group->base.ring_mask = -1;
+	group->base.dial_mask = -1;
 	group->base.toggle_button_mask = 0;
 
 	list_insert(&pad->modes.mode_group_list, &group->base.link);

src/evdev-tablet-pad.h
@@ -41,6 +41,8 @@ enum pad_axes {
 	PAD_AXIS_RING2		= bit(1),
 	PAD_AXIS_STRIP1		= bit(2),
 	PAD_AXIS_STRIP2		= bit(3),
+	PAD_AXIS_DIAL1		= bit(4),
+	PAD_AXIS_DIAL2		= bit(5),
 };
 
 struct button_state {
@@ -72,6 +74,12 @@ struct pad_dispatch {
 	unsigned int nbuttons;
 
 	bool have_abs_misc_terminator;
+
+	struct {
+		bool has_hires_dial;
+		double dial1;
+		double dial2;
+	} dials;
 
 	struct {
 		struct libinput_device_config_send_events config;

src/evdev.h
@@ -519,6 +519,9 @@ int
 evdev_device_tablet_pad_get_num_buttons(struct evdev_device *device);
 
 int
+evdev_device_tablet_pad_get_num_dials(struct evdev_device *device);
+
+int
 evdev_device_tablet_pad_get_num_rings(struct evdev_device *device);
 
 int

src/libinput-private.h
@@ -503,6 +503,7 @@ struct libinput_tablet_pad_mode_group {
 	uint32_t button_mask;
 	uint32_t ring_mask;
 	uint32_t strip_mask;
+	uint32_t dial_mask;
 
 	uint32_t toggle_button_mask;
 
@@ -796,6 +797,13 @@ tablet_pad_notify_button(struct libinput_device *device,
 			 int32_t button,
 			 enum libinput_button_state state,
 			 struct libinput_tablet_pad_mode_group *group);
+void
+tablet_pad_notify_dial(struct libinput_device *device,
+		       uint64_t time,
+		       unsigned int number,
+		       double value,
+		       struct libinput_tablet_pad_mode_group *group);
+
 void
 tablet_pad_notify_ring(struct libinput_device *device,
 		       uint64_t time,

src/libinput.c
@@ -104,6 +104,7 @@ event_type_to_str(enum libinput_event_type type)
 	CASE_RETURN_STRING(LIBINPUT_EVENT_TABLET_PAD_RING);
 	CASE_RETURN_STRING(LIBINPUT_EVENT_TABLET_PAD_STRIP);
 	CASE_RETURN_STRING(LIBINPUT_EVENT_TABLET_PAD_KEY);
+	CASE_RETURN_STRING(LIBINPUT_EVENT_TABLET_PAD_DIAL);
 	CASE_RETURN_STRING(LIBINPUT_EVENT_GESTURE_SWIPE_BEGIN);
 	CASE_RETURN_STRING(LIBINPUT_EVENT_GESTURE_SWIPE_UPDATE);
 	CASE_RETURN_STRING(LIBINPUT_EVENT_GESTURE_SWIPE_END);
@@ -232,6 +233,10 @@ struct libinput_event_tablet_pad {
 		uint32_t code;
 		enum libinput_key_state state;
 	} key;
+	struct {
+		double v120;
+		int number;
+	} dial;
 	struct {
 		enum libinput_tablet_pad_ring_axis_source source;
 		double position;
@@ -443,6 +448,7 @@ libinput_event_get_tablet_pad_event(struct libinput_event *event)
 			   event->type,
 			   NULL,
 			   LIBINPUT_EVENT_TABLET_PAD_RING,
+			   LIBINPUT_EVENT_TABLET_PAD_DIAL,
 			   LIBINPUT_EVENT_TABLET_PAD_STRIP,
 			   LIBINPUT_EVENT_TABLET_PAD_BUTTON,
 			   LIBINPUT_EVENT_TABLET_PAD_KEY);
@@ -2024,6 +2030,7 @@ libinput_event_destroy(struct libinput_event *event)
 		   libinput_event_get_tablet_tool_event(event));
 		break;
 	case LIBINPUT_EVENT_TABLET_PAD_RING:
+	case LIBINPUT_EVENT_TABLET_PAD_DIAL:
 	case LIBINPUT_EVENT_TABLET_PAD_STRIP:
 	case LIBINPUT_EVENT_TABLET_PAD_BUTTON:
 	case LIBINPUT_EVENT_TABLET_PAD_KEY:
@@ -2910,6 +2917,34 @@ tablet_pad_notify_button(struct libinput_device *device,
 }
 
 void
+tablet_pad_notify_dial(struct libinput_device *device,
+		       uint64_t time,
+		       unsigned int number,
+		       double value,
+		       struct libinput_tablet_pad_mode_group *group)
+{
+	struct libinput_event_tablet_pad *dial_event;
+	unsigned int mode;
+
+	dial_event = zalloc(sizeof *dial_event);
+
+	mode = libinput_tablet_pad_mode_group_get_mode(group);
+
+	*dial_event = (struct libinput_event_tablet_pad) {
+		.time = time,
+		.dial.number = number,
+		.dial.v120 = value,
+		.mode_group = libinput_tablet_pad_mode_group_ref(group),
+		.mode = mode,
+	};
+
+	post_device_event(device,
+			  time,
+			  LIBINPUT_EVENT_TABLET_PAD_DIAL,
+			  &dial_event->base);
+}
+
+void
 tablet_pad_notify_ring(struct libinput_device *device,
 		       uint64_t time,
 		       unsigned int number,
@@ -3376,6 +3411,12 @@ libinput_device_tablet_pad_get_num_buttons(struct libinput_device *device)
 }
 
 LIBINPUT_EXPORT int
+libinput_device_tablet_pad_get_num_dials(struct libinput_device *device)
+{
+	return evdev_device_tablet_pad_get_num_dials((struct evdev_device *)device);
+}
+
+LIBINPUT_EXPORT int
 libinput_device_tablet_pad_get_num_rings(struct libinput_device *device)
 {
 	return evdev_device_tablet_pad_get_num_rings((struct evdev_device *)device);
@@ -3429,6 +3470,17 @@ libinput_tablet_pad_mode_group_has_button(struct libinput_tablet_pad_mode_group 
 		return 0;
 
 	return !!(group->button_mask & bit(button));
+}
+
+LIBINPUT_EXPORT int
+libinput_tablet_pad_mode_group_has_dial(struct libinput_tablet_pad_mode_group *group,
+					unsigned int dial)
+{
+	if ((int)dial >=
+	    libinput_device_tablet_pad_get_num_dials(group->device))
+		return 0;
+
+	return !!(group->dial_mask & bit(dial));
 }
 
 LIBINPUT_EXPORT int
@@ -3590,6 +3642,28 @@ libinput_event_tablet_tool_get_base_event(struct libinput_event_tablet_tool *eve
 }
 
 LIBINPUT_EXPORT double
+libinput_event_tablet_pad_get_dial_delta_v120(struct libinput_event_tablet_pad *event)
+{
+	require_event_type(libinput_event_get_context(&event->base),
+			   event->base.type,
+			   0.0,
+			   LIBINPUT_EVENT_TABLET_PAD_DIAL);
+
+	return event->dial.v120;
+}
+
+LIBINPUT_EXPORT unsigned int
+libinput_event_tablet_pad_get_dial_number(struct libinput_event_tablet_pad *event)
+{
+	require_event_type(libinput_event_get_context(&event->base),
+			   event->base.type,
+			   0,
+			   LIBINPUT_EVENT_TABLET_PAD_DIAL);
+
+	return event->dial.number;
+}
+
+LIBINPUT_EXPORT double
 libinput_event_tablet_pad_get_ring_position(struct libinput_event_tablet_pad *event)
 {
 	require_event_type(libinput_event_get_context(&event->base),
@@ -3706,6 +3780,7 @@ libinput_event_tablet_pad_get_mode(struct libinput_event_tablet_pad *event)
 			   event->base.type,
 			   0,
 			   LIBINPUT_EVENT_TABLET_PAD_RING,
+			   LIBINPUT_EVENT_TABLET_PAD_DIAL,
 			   LIBINPUT_EVENT_TABLET_PAD_STRIP,
 			   LIBINPUT_EVENT_TABLET_PAD_BUTTON);
 
@@ -3719,6 +3794,7 @@ libinput_event_tablet_pad_get_mode_group(struct libinput_event_tablet_pad *event
 			   event->base.type,
 			   NULL,
 			   LIBINPUT_EVENT_TABLET_PAD_RING,
+			   LIBINPUT_EVENT_TABLET_PAD_DIAL,
 			   LIBINPUT_EVENT_TABLET_PAD_STRIP,
 			   LIBINPUT_EVENT_TABLET_PAD_BUTTON);
 
@@ -3732,6 +3808,7 @@ libinput_event_tablet_pad_get_time(struct libinput_event_tablet_pad *event)
 			   event->base.type,
 			   0,
 			   LIBINPUT_EVENT_TABLET_PAD_RING,
+			   LIBINPUT_EVENT_TABLET_PAD_DIAL,
 			   LIBINPUT_EVENT_TABLET_PAD_STRIP,
 			   LIBINPUT_EVENT_TABLET_PAD_BUTTON,
 			   LIBINPUT_EVENT_TABLET_PAD_KEY);
@@ -3746,6 +3823,7 @@ libinput_event_tablet_pad_get_time_usec(struct libinput_event_tablet_pad *event)
 			   event->base.type,
 			   0,
 			   LIBINPUT_EVENT_TABLET_PAD_RING,
+			   LIBINPUT_EVENT_TABLET_PAD_DIAL,
 			   LIBINPUT_EVENT_TABLET_PAD_STRIP,
 			   LIBINPUT_EVENT_TABLET_PAD_BUTTON,
 			   LIBINPUT_EVENT_TABLET_PAD_KEY);
@@ -3760,6 +3838,7 @@ libinput_event_tablet_pad_get_base_event(struct libinput_event_tablet_pad *event
 			   event->base.type,
 			   NULL,
 			   LIBINPUT_EVENT_TABLET_PAD_RING,
+			   LIBINPUT_EVENT_TABLET_PAD_DIAL,
 			   LIBINPUT_EVENT_TABLET_PAD_STRIP,
 			   LIBINPUT_EVENT_TABLET_PAD_BUTTON,
 			   LIBINPUT_EVENT_TABLET_PAD_KEY);

src/libinput.h
@@ -164,7 +164,8 @@ struct libinput_event_tablet_tool;
  *
  * Tablet pad event representing a button press, or ring/strip update on
  * the tablet pad itself. Valid event types for this event are @ref
- * LIBINPUT_EVENT_TABLET_PAD_BUTTON, @ref LIBINPUT_EVENT_TABLET_PAD_RING and
+ * LIBINPUT_EVENT_TABLET_PAD_BUTTON, @ref LIBINPUT_EVENT_TABLET_PAD_DIAL,
+ * @ref LIBINPUT_EVENT_TABLET_PAD_RING and
  * @ref LIBINPUT_EVENT_TABLET_PAD_STRIP.
  *
  * @since 1.3
@@ -429,7 +430,8 @@ struct libinput_tablet_pad_mode_group;
  * the Wacom Cintiq 22HD provide two mode groups. If multiple mode groups
  * are available, a caller should use
  * libinput_tablet_pad_mode_group_has_button(),
- * libinput_tablet_pad_mode_group_has_ring() and
+ * libinput_tablet_pad_mode_group_has_ring(),
+ * libinput_tablet_pad_mode_group_has_dial() and
  * libinput_tablet_pad_mode_group_has_strip() to associate each button,
  * ring and strip with the correct mode group.
  *
@@ -538,6 +540,22 @@ libinput_tablet_pad_mode_group_get_mode(struct libinput_tablet_pad_mode_group *g
 int
 libinput_tablet_pad_mode_group_has_button(struct libinput_tablet_pad_mode_group *group,
 					  unsigned int button);
+
+/**
+ * @ingroup tablet_pad_modes
+ *
+ * Devices without mode switching capabilities return true for every dial.
+ *
+ * @param group A previously obtained mode group
+ * @param dial A dial index, starting at 0
+ * @return true if the given dial index is part of this mode group or
+ * false otherwise
+ *
+ * @since 1.26
+ */
+int
+libinput_tablet_pad_mode_group_has_dial(struct libinput_tablet_pad_mode_group *group,
+					unsigned int dial);
 
 /**
  * @ingroup tablet_pad_modes
@@ -967,6 +985,14 @@ enum libinput_event_type {
 	 * @since 1.15
 	 */
 	LIBINPUT_EVENT_TABLET_PAD_KEY,
+
+	/**
+	 * A status change on a tablet dial with the @ref
+	 * LIBINPUT_DEVICE_CAP_TABLET_PAD capability.
+	 *
+	 * @since 1.26
+	 */
+	LIBINPUT_EVENT_TABLET_PAD_DIAL,
 
 	LIBINPUT_EVENT_GESTURE_SWIPE_BEGIN = 800,
 	LIBINPUT_EVENT_GESTURE_SWIPE_UPDATE,
@@ -3317,6 +3343,44 @@ libinput_event_tablet_pad_get_key_state(struct libinput_event_tablet_pad *event)
 /**
  * @ingroup event_tablet_pad
  *
+ * Returns the delta change of the dial, in multiples or fractions of 120, with
+ * each multiple of 120 indicating one logical wheel event.
+ * See libinput_event_pointer_get_scroll_value_v120() for more details.
+ *
+ * @note It is an application bug to call this function for events other than
+ * @ref LIBINPUT_EVENT_TABLET_PAD_DIAL.  For other events, this function
+ * returns 0.
+ *
+ * @param event The libinput tablet pad event
+ * @return The delta of the the axis
+ *
+ * @since 1.26
+ */
+double
+libinput_event_tablet_pad_get_dial_delta_v120(struct libinput_event_tablet_pad *event);
+
+/**
+ * @ingroup event_tablet_pad
+ *
+ * Returns the number of the dial that has changed state, with 0 being the
+ * first dial. On tablets with only one dial, this function always returns
+ * 0.
+ *
+ * @note It is an application bug to call this function for events other than
+ * @ref LIBINPUT_EVENT_TABLET_PAD_DIAL.  For other events, this function
+ * returns 0.
+ *
+ * @param event The libinput tablet pad event
+ * @return The index of the dial that changed state
+ *
+ * @since 1.26
+ */
+unsigned int
+libinput_event_tablet_pad_get_dial_number(struct libinput_event_tablet_pad *event);
+
+/**
+ * @ingroup event_tablet_pad
+ *
  * Returns the mode the button, ring, or strip that triggered this event is
  * in, at the time of the event.
  *
@@ -4376,6 +4440,23 @@ libinput_device_switch_has_switch(struct libinput_device *device,
  */
 int
 libinput_device_tablet_pad_get_num_buttons(struct libinput_device *device);
+
+/**
+ * @ingroup device
+ *
+ * Return the number of dials a device with the @ref
+ * LIBINPUT_DEVICE_CAP_TABLET_PAD capability provides.
+ *
+ * @param device A current input device
+ *
+ * @return The number of dials or 0 if the device has no dials. -1 on error.
+ *
+ * @see libinput_event_tablet_pad_get_dial_number
+ *
+ * @since 1.26
+ */
+int
+libinput_device_tablet_pad_get_num_dials(struct libinput_device *device);
 
 /**
  * @ingroup device

commit 11ff5c81cc0454081f35c51869869a4245ab7cdf

tablet: Remove unnecessary scale factor on REL_HWHEEL_HI_RES
The two high-res axes should already be scaled appropriately by the
kernel. This unnecessary scale factor causes 1 click of the dial to
produce an event delta of +-14400 rather than the expected +-120.

Fixes: e2cc4456648c ("tablet: add API for relative dials")
Part-of: <https://gitlab.freedesktop.org/libinput/libinput/-/merge_requests/1029>

src/evdev-tablet-pad.c
@@ -129,7 +129,7 @@ pad_process_relative(struct pad_dispatch *pad,
 		pad_set_status(pad, PAD_AXES_UPDATED);
 		break;
 	case REL_HWHEEL_HI_RES:
-		pad->dials.dial2 = e->value * 120;
+		pad->dials.dial2 = e->value;
 		pad->changed_axes |= PAD_AXIS_DIAL2;
 		pad_set_status(pad, PAD_AXES_UPDATED);
 		break;