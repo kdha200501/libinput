kdha200501/libinput

commit 3544d78a92a11e32b78c2f47074f6cc05679bd19

tools/libinput-replay: Warn if writing to local-overrides fails
Closes: https://gitlab.freedesktop.org/libinput/libinput/-/issues/1153
Part-of: <https://gitlab.freedesktop.org/libinput/libinput/-/merge_requests/1264>

tools/libinput-replay.py
@@ -337,10 +337,14 @@ def setup_quirks(recording):
         sys.exit(1)
 
     overrides.parent.mkdir(exist_ok=True)
-    with overrides.open("w+") as fd:
-        fd.write("# This file was generated by libinput replay\n")
-        fd.write("# Unless libinput replay is running right now, remove this file.\n")
-        fd.write("\n\n".join(quirks))
+    try:
+        with overrides.open("w+") as fd:
+            fd.write("# This file was generated by libinput replay\n")
+            fd.write("# Unless libinput replay is running right now, remove this file.\n")
+            fd.write("\n\n".join(quirks))
+    except IOError:
+        error("Warning: Couldn't write to {}, please re-run as root".format(overrides))
+        return None
 
     return overrides
 

commit ba38579e2d51112ef1486fa4cefe24f0f94fd4a5

tools: fix ruff format for libinput-replay
Fixes: 3544d78a92a1 ("tools/libinput-replay: Warn if writing to local-overrides fails")
Part-of: <https://gitlab.freedesktop.org/libinput/libinput/-/merge_requests/1272>

tools/libinput-replay.py
@@ -340,7 +340,9 @@ def setup_quirks(recording):
     try:
         with overrides.open("w+") as fd:
             fd.write("# This file was generated by libinput replay\n")
-            fd.write("# Unless libinput replay is running right now, remove this file.\n")
+            fd.write(
+                "# Unless libinput replay is running right now, remove this file.\n"
+            )
             fd.write("\n\n".join(quirks))
     except IOError:
         error("Warning: Couldn't write to {}, please re-run as root".format(overrides))